a2f56f9ef65811300df6ba8d3b4dfedce2e250e6
siva263 <130476034+siva263@users.noreply.github.com>
2024-05-20 19:58:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -117,4 +117,4 @@ def send_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(debug=True)
