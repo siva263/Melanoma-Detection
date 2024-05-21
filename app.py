bc037fe255b421af289c34c7fcc3556a637a4042
siva263 <130476034+siva263@users.noreply.github.com>
2024-05-21 17:18:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,13 +12,14 @@ app = Flask(__name__)
 UPLOAD_FOLDER = "uploads/all_class"
 STATIC_FOLDER = "static"
 MODEL_FILENAME = "VGG19-224-model.06-0.12.hdf5"
-MODEL_PATH = os.path.join(STATIC_FOLDER, MODEL_FILENAME)
+
 GOOGLE_DRIVE_FILE_ID = '1GcI419Ev7zwrSpWnEHVg4xDYI4kyuj-b'
 
 ALLOWED_EXTENSIONS = {'jpg', 'jpeg'}
 IMAGE_SIZE = 224
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 
 def download_file_from_google_drive(file_id, dest_path):
     URL = "https://drive.google.com/uc?export=download"
@@ -33,11 +34,13 @@ def download_file_from_google_drive(file_id, dest_path):
 
     save_response_content(response, dest_path)
 
+
 def get_confirm_token(response):
     for key, value in response.cookies.items():
         if key.startswith('download_warning'):
             return value
     return None
+
 
 def save_response_content(response, dest_path):
     CHUNK_SIZE = 32768
@@ -46,14 +49,20 @@ def save_response_content(response, dest_path):
             if chunk:
                 f.write(chunk)
 
-# Check if the model file exists, if not download it
-if not os.path.exists(MODEL_PATH):
-    print(f"Downloading model to {MODEL_PATH}...")
-    download_file_from_google_drive(GOOGLE_DRIVE_FILE_ID, MODEL_PATH)
-    print(f"Downloaded model to {MODEL_PATH}")
 
-# Load the model
-model = tf.keras.models.load_model(MODEL_PATH, compile=False)
+# This section checks if a model file exists. 
+# If not, it downloads it from Google Drive during deployment  
+model_path = os.path.join(STATIC_FOLDER, MODEL_FILENAME)
+if not os.path.exists(model_path):
+    print(f"Downloading model to {model_path}...")
+    download_file_from_google_drive(GOOGLE_DRIVE_FILE_ID, model_path)
+    print(f"Downloaded model to {model_path}")
+
+# Load the model only if it exists (prevents initial error)
+if os.path.exists(model_path):
+    model = tf.keras.models.load_model(model_path, compile=False)
+else:
+    print("Model file not found. Please ensure the model is downloaded or the path is correct.")
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
